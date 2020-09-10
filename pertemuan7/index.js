f5d9a600610d35bbdb54d8f577276e5ce1b7b3b8
brightrore <55672104+brightrore@users.noreply.github.com>
2020-09-10 21:46:12 +0800

    conditional dan looping

---
+++ pertemuan7/index.js
@@ -0,0 +1,98 @@
+//#conditional menggunakan If, else if dan else
+//let nilai1 = 75;
+//if (nilai >= 80 && nilai <=85) {
+//  console.log("B+");
+//} else if (nilai >= 75 && nilai <= 79) {
+//  console.log("B");
+//} else {
+//  console.log("WP");
+//}
+
+//#conditional menggunakan switch case
+//let jadwal;
+// let hari = "minggu";
+
+// switch (hari) {
+//   case "senin":
+//     mataPelajaran = "Matematika, Fisika";
+//     break;
+//   case "selasa":
+//     mataPelajaran = "Kimia, Biologi";
+//     break;
+//   case "rabu":
+//     mataPelajaran = "Bhs. indo, Bhs.inggris";
+//     break;
+//   case "kamis":
+//     mataPelajaran = "Sejarah, PKN";
+//     break;
+//   case "jumat":
+//     mataPelajaran = "Agama, Penjas";
+//     break;
+//   default:
+//     mataPelajaran = "Libur";
+// }
+
+// console.log(mataPelajaran);
+
+// let mataPelajaran;
+// let hari = "senin";
+
+// switch (hari) {
+//   case "senin":
+//   case "rabu":
+//       mataPelajaran = "comgraf, skripsi2";
+//       break;
+//   case "selasa":
+//   case "kamis":
+//     mataPelajaran = "webpro, KM";
+//     break;
+//   case "jumat":
+//     mataPelajaran = "free";
+//     break;
+//   default:
+//     mataPelajaran = "weekend";
+// }
+
+// console.log(mataPelajaran); 
+
+// #for loop
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+  
+// for/in loop
+// const buku = {
+//     judul: "Harry Potter and The Philosopher's Stone",
+//     pengarang: "J. K. Rowling",
+//     tahun: 1997
+//  };
+//  for (x in buku) {
+//    console.log(x, ':', buku[x]);
+//  }
+ 
+// for/of loop
+// const buku = ["Game of Thrones: A Song of Ice and Fire",  
+//    "Harry Potter and The Philosopher's Stone", 
+//    "Lord of The Rings: The Fellowship of The Ring"
+// ];
+
+// for (x of buku) {
+//   console.log(x);
+// }
+
+// while loop
+// let i = 1;
+
+// while (i <= 11) {
+//   console.log(i);
+//   i++;
+// }
+
+// do while loop
+// let i = 1;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
+
