b2b19512714038aba47506fe970a5d78cfdfdc47
brightrore <55672104+brightrore@users.noreply.github.com>
2020-09-22 13:41:52 +0800

    exercise2

---
+++ pertemuan10/index.js
@@ -0,0 +1,13 @@
+// for(let i=0; i<10; i++){
+//     console.log(i)
+// }
+
+// tamplate literal
+const person= {
+    fisrtName : "Bright",
+    lastname : "rore",
+    age : 20,
+}
+console.log(
+    `halo nama saya ${person.fisrtName} ${person.lastname}, dan umur saya ${person.age}`
+)
