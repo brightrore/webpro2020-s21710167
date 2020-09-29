cf33dddd51219282ad7329e848f639db10239474
brightrore <55672104+brightrore@users.noreply.github.com>
2020-09-29 14:47:34 +0800

    parameter

---
+++ pertemuan12/index.js
@@ -0,0 +1,36 @@
+// Arrow Function
+
+// const calcAge = (year) => {
+//     return 2020-year;
+// };
+
+// console.log(calcAge(1991));
+
+// implicit return value
+// const calcAge = () => 2020-1991;
+// console.log(calcAge());
+
+// const years = [1990, 1992, 2001, 2005];
+// const calcAge5 = years.map(function (el) {
+//     return 2020-el;
+// }); // kse lanjut
+
+// Default parameter
+
+// const calcAge = (birthyear, currentyear = 2020) => currentyear-birthyear;
+
+// console.log(calcAge(1991));
+
+// exxercise
+// convert to ES6 syntax
+// function yearUntilRetirement(year, firstName){
+//     var age=28;
+//     var retirement=65-age;
+//     if(retirement>0){
+//         console.log(firstName + " retired in " + retirement + " years")
+//     } else{
+//         console.log(firstName + " is already retired.");
+//     }
+// }
+// console.log(yearUntilRetirement(2020,"Bright"));
+
