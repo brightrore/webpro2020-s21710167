63b19a9f50296d0fa4552b01c368b6164893fec7
brightrore <55672104+brightrore@users.noreply.github.com>
2020-11-03 14:23:34 +0800

    ujian02

--- ujian02/index.js
+++ ujian02/index.js
@@ -15,38 +15,48 @@
 // yearUntilRetirement({year: 1987, firstName: 'John'});
 
 // no.2
-const addNumber = (...numbers) => {
-    let sum = 0;
-    numbers.map(i => (sum+=i));
-    return sum;
-}
+// const addNumber = (...numbers) => {
+//     let sum = 0;
+//     numbers.map(i => (sum+=i));
+//     return sum;
+// }
 
-console.log(addNumber(1,2,3,4,5,6,7));
+// console.log(addNumber(1,2,3,4,5,6,7));
 
 // No.3
-// var phi = 3.14;
-// var power = 2;
-// var radius = 0;
+// let phi = 3.14;
+// let power = 2;
+// let radius = 0;
  
-// var calculateArea = function (obj) {
-//   return phi * Math.pow(obj.radius, obj.power);
-// };
+// const calculateArea = obj => phi * Math.pow(obj.radius, obj.power);
  
 // radius = 21;
-// var area21 = calculateArea({radius: radius, power: 2 });
+// let area21 = calculateArea({radius: radius, power: power });
  
 // radius = 7;
-// var area7 = calculateArea({radius: radius, power: 2 });
+// let area7 = calculateArea({radius: radius, power: power });
  
-// console.log('area with 21 radius: ' + area21 + ', and area with 7 radius: ' + area7);
+// console.log(`area with 21 radius: ${area21}, and area with 7 radius: ${area7}`);
 
 // no.4
-// onst makeAjaxRequest = (url, method) => {
-//     if(!method){
-//            method = ‘GET’
-//     }
+// const makeAjaxRequest = (url, method = "GET") => {
 //     console.log(url, method);
-//     }
-     
-//     makeAjaxRequest(‘www.google.com’);
+// };
+// makeAjaxRequest("www.google.com");
+
+// bagian2
+// no1
+// const warnaKesukaan = (warna = "biru") =>
+//     console.log(`Warna Kesukaan Saya adalah ${warna}`);
+// warnaKesukaan();
+
+// no2
+// let namaBuah1 = ["apel", "mangga"];
+// let namaBuah2 = ["sirsak", "semangka"];
+// let semuaNamaBuah;
+
+// semuaNamaBuah = [...namaBuah1, ...namaBuah2];
+
+// console.log(semuaNamaBuah);
+
     
