bf3224de32586bd3173c9d178e4988b7b8910146
brightrore <55672104+brightrore@users.noreply.github.com>
2020-11-03 14:42:45 +0800

    ujian02

--- ujian02/index.js
+++ ujian02/index.js
@@ -24,39 +24,39 @@
 // console.log(addNumber(1,2,3,4,5,6,7));
 
 // No.3
-// let phi = 3.14;
-// let power = 2;
-// let radius = 0;
+let phi = 3.14;
+let power = 2;
+let radius = 0;
  
-// const calculateArea = obj => phi * Math.pow(obj.radius, obj.power);
+const calculateArea = obj => phi * Math.pow(obj.radius, obj.power);
  
-// radius = 21;
-// let area21 = calculateArea({radius: radius, power: power });
+radius = 21;
+let area21 = calculateArea({radius: radius, power: power });
  
-// radius = 7;
-// let area7 = calculateArea({radius: radius, power: power });
+radius = 7;
+let area7 = calculateArea({radius: radius, power: power });
  
-// console.log(`area with 21 radius: ${area21}, and area with 7 radius: ${area7}`);
+console.log(`area with 21 radius: ${area21}, and area with 7 radius: ${area7}`);
 
 // no.4
-// const makeAjaxRequest = (url, method = "GET") => {
-//     console.log(url, method);
-// };
-// makeAjaxRequest("www.google.com");
+const makeAjaxRequest = (url, method = "GET") => {
+    console.log(url, method);
+};
+makeAjaxRequest("www.google.com");
 
-// bagian2
-// no1
-// const warnaKesukaan = (warna = "biru") =>
-//     console.log(`Warna Kesukaan Saya adalah ${warna}`);
-// warnaKesukaan();
+bagian2
+no1
+const warnaKesukaan = (warna = "biru") =>
+    console.log(`Warna Kesukaan Saya adalah ${warna}`);
+warnaKesukaan();
 
 // no2
-// let namaBuah1 = ["apel", "mangga"];
-// let namaBuah2 = ["sirsak", "semangka"];
-// let semuaNamaBuah;
+let namaBuah1 = ["apel", "mangga"];
+let namaBuah2 = ["sirsak", "semangka"];
+let semuaNamaBuah;
 
-// semuaNamaBuah = [...namaBuah1, ...namaBuah2];
+semuaNamaBuah = [...namaBuah1, ...namaBuah2];
 
-// console.log(semuaNamaBuah);
+console.log(semuaNamaBuah);
 
     
